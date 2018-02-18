265ba6490c493f6b0a7d944adba92d251b17074f
Isabel O <kidlatmc@gmail.com>
2018-02-17 19:29:18 -0800

    Added imports

--- src/com/krysabel/sort/Main.java
+++ src/com/krysabel/sort/Main.java
@@ -1,9 +1,17 @@
 package com.krysabel.sort;
-public class Main {
+import java.io.Console;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List; 
 
+public class Main {
+	ArrayList<String> people = new ArrayList<String>();
 	public static void main(String[] args) {
 		
-		System.out.println("Testing commiting and pushing!");
+		System.out.println("Hello! Welcome to the random raffle picker.");
+		System.out.println("Please enter the name of the file you want to use: ");
+		
+		
 	}
 
 }
