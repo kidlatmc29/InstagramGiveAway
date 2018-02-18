0ecbb702c7e82d2b23b691c68af8a135cddb6f98
Isabel O <kidlatmc@gmail.com>
2018-02-17 20:32:30 -0800

    Added getInputfile();

--- src/com/krysabel/sort/Main.java
+++ src/com/krysabel/sort/Main.java
@@ -1,17 +1,42 @@
 package com.krysabel.sort;
-import java.io.Console;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List; 
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	ArrayList<String> people = new ArrayList<String>();
 	public static void main(String[] args) {
-		
+		Scanner userInput = new Scanner(System.in); 
 		System.out.println("Hello! Welcome to the random raffle picker.");
 		System.out.println("Please enter the name of the file you want to use: ");
 		
-		
+		boolean fileExists = false; 
+		File inputFile = getInputFile(userInput, fileExists);
+	}
+	
+	/**
+	 * Private method that gets an input file name from user.
+	 * 
+	 * @param console
+	 * @param fileExists
+	 * @return inputFile
+	 */
+	private static File getInputFile(Scanner console, boolean fileExists) {
+		File inputFile = null;
+
+		while (!fileExists) {
+			System.out.print("> Please enter input file name with file extention: ");
+			String inputFileName = console.next();
+
+			inputFile = new File(inputFileName.trim());
+
+			fileExists = inputFile.exists();
+			if (!inputFile.exists()) {
+
+				System.out.println("> " + inputFileName + " does not exist!");
+			}
+
+		}
+		return inputFile;
 	}
 
 }
