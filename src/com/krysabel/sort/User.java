3b19a2a205b61a7b0ba9aa166ad2132db01ac33d
ozabell <kidlatmc@gmail.com>
2019-06-27 19:59:56 -0700

    trying to rework this problem with oop

---
+++ src/com/krysabel/sort/User.java
@@ -0,0 +1,13 @@
+package com.krysabel.sort;
+
+public class User {
+	private String mName;
+	private int mNumberOfEntries; 
+	
+	public User(String name, int numberOfEntries) {
+		mName = name;
+		mNumberOfEntries = numberOfEntries; 
+	}
+	
+	
+}
